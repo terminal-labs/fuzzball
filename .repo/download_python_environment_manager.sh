6064977044979caeb614848ad6149a4fb687f345
Michael Verhulst <michael@terminallabs.com>
2020-11-26 20:07:07 -0600

    Update download_python_environment_manager.sh

--- .repo/download_python_environment_manager.sh
+++ .repo/download_python_environment_manager.sh
@@ -1,7 +1,8 @@
+apt install -y wget unzip make build-essential
 su -m vagrant <<'EOF'
   cd /vagrant
   mkdir -p .tmp
   cd .tmp
-  wget https://github.com/terminal-labs/bash-environment-manager/archive/master.zip
-  unzip master.zip
+  wget -O bash-environment-manager.zip https://github.com/terminal-labs/bash-environment-manager/archive/master.zip  
+  unzip bash-environment-manager.zip
 EOF
