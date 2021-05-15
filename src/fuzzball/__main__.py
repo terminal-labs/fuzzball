7614a2590e82cbc922c6c1804e755eec177c5b5b
verhulstm <michael@terminallabs.com>
2021-05-15 14:26:44 -0500

    more meta

--- src/fuzzball/__main__.py
+++ src/fuzzball/__main__.py
@@ -1,3 +1,8 @@
-from fuzzball.cli import main
+import os
 
-main()
+with open(os.path.dirname(__file__) + "/framework/lib.py") as f:
+    code = compile(f.read(), "lib.py", "exec")
+    exec(code)
+
+_pgk_name = _get_pgk_name()
+main = _import_fun(f"{_pgk_name}.cli", "main")
